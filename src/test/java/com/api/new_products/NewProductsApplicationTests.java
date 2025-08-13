38ed36c1678f2b9e6a334afa51800740be6f4e3e
rafaelneeves <rafaelfernando37@gmail.com>
2025-08-13 08:48:36 -0300

    Add java and spring boot

---
+++ src/test/java/com/api/new_products/NewProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.new_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
